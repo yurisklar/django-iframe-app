4ec1f640d138ce8b5501cf14241716e35703d322
yurisklar <yuri.sklar@gmail.com>
2014-11-01 21:43:23 +0200

    a bit nicer admin for tags/images view

--- project/iframe/models.py
+++ project/iframe/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class ImageTag(models.Model):
     name = models.CharField(max_length=64)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Image(models.Model):
     tag = models.ForeignKey(ImageTag)
     picture = models.ImageField(upload_to='images')
+
+    def __unicode__(self):
+        return "Image with tag %s" % (self.tag.name, )
