495043417e8c1ea30e2af231e71b53bab134fc00
yurisklar <yuri.sklar@gmail.com>
2014-11-01 21:26:12 +0200

    the default loader images will be received from server

--- project/static/js/apps/iframe/app.js
+++ project/static/js/apps/iframe/app.js
@@ -9,25 +9,7 @@
     app.imagesView = {};
 
 	$(function(){
-
-        var imagesCollection = new app.collections.Images([
-            {
-                image: 'http://image.shutterstock.com/display_pic_with_logo/2330918/198495395/stock-vector-set-of-loading-symbols-198495395.jpg'
-            },
-            {
-                image: 'http://image.shutterstock.com/display_pic_with_logo/2330918/198495395/stock-vector-set-of-loading-symbols-198495395.jpg'
-            },
-            {
-                image: 'http://image.shutterstock.com/display_pic_with_logo/2330918/198495395/stock-vector-set-of-loading-symbols-198495395.jpg'
-            },
-            {
-                image: 'http://image.shutterstock.com/display_pic_with_logo/2330918/198495395/stock-vector-set-of-loading-symbols-198495395.jpg'
-            },
-            {
-                image: 'http://image.shutterstock.com/display_pic_with_logo/2330918/198495395/stock-vector-set-of-loading-symbols-198495395.jpg'
-            }
-        ]);
-
+        var imagesCollection = new app.collections.Images({});
         app.imagesView = new app.views.ImagesView({collection: imagesCollection});
 		new app.router.ApiRouter();
         Backbone.history.start({root: "/iframe/"});
--- project/static/js/apps/iframe/router.js
+++ project/static/js/apps/iframe/router.js
@@ -5,29 +5,32 @@
             "tag/:tag_name": "set_tag"
         },
         set_tag: function(tag_name){
-            // after new tag is set using location.hash
-            console.log("set tag");
-            console.log(tag_name);
-
-            // first show loading
-
+            // first show loading images
+            this.set_loading_images();
+            // and now load necessary images
             app.imagesView.collection.url = '/images/' + tag_name;
             app.imagesView.collection.fetch({
                 url: '/images/' + tag_name,
                 success: function (collection) {
-                    app.imagesView.collection = collection;
-                    app.imagesView.render();
-                }
+                    this.set_images_collection(collection)
+                }.bind(this)
             });
         },
         initialize: function(){
-            console.log("initialize");
+            // first show loading images
             return this.set_loading_images();
-            // set default images
+        },
+        set_images_collection: function(collection){
+            app.imagesView.collection = collection;
+            app.imagesView.render();
         },
         set_loading_images: function(){
+            var default_array = []
+            for (var i = 0; i < 5; i++){
+                default_array.push(window.loading_image_obj);
+            }
+            app.imagesView.collection.reset(default_array);
             app.imagesView.render();
-            // here we render default "loading" images
         }
     });
 })(app.router, app.imagesView);
