99fa19f65d526176f56f0350c8ab6966329832b6
yurisklar <yuri.sklar@gmail.com>
2014-11-01 22:05:16 +0200

    handle AJAX GET requests to fetch images by tag

--- project/iframe/views.py
+++ project/iframe/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+import json
 
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View
+from django.views.generic.base import ContextMixin
 
+from iframe.models import Image
 
 class IframeView(TemplateView):
     template_name = "iframe.html"
+
+
+class ImagesView(View, ContextMixin):
+    http_method_names = ['get']
+    content_type = "application/json"
+
+    def get_context_data(self, **kwargs):
+        images = Image.objects.filter(tag__name=kwargs["tag"])
+        if not images:
+            images = Image.objects.order_by('?')[0:1]
+        return images
+
+    def render_to_json_response(self, context):
+        return HttpResponse(self.convert_context_to_json(context))
+
+    def convert_context_to_json(self, context):
+        return json.dumps([{"image": image.picture.url} for image in context])
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_json_response(context)
--- project/project/urls.py
+++ project/project/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
-from iframe.views import IframeView
+from iframe.views import IframeView, ImagesView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -11,6 +11,10 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('',
     url(r'^iframe/', IframeView.as_view()),
+)
+
+urlpatterns += patterns('',
+    url(r'^images/(?P<tag>.*)/$', ImagesView.as_view()),
 )
 
 if settings.DEBUG:
