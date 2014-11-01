674b9c34b89e5c317b6ec537a261fd20c4b2f972
yurisklar <yuri.sklar@gmail.com>
2014-11-01 18:43:56 +0200

    adding the settings to have static and media files serving

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from iframe.views import IframeView
 
@@ -11,4 +13,9 @@ urlpatterns += patterns('',
     url(r'^iframe/', IframeView.as_view()),
 )
 
-
+if settings.DEBUG:
+    # serve static files by django only in development mode
+    urlpatterns += patterns('',
+    ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += patterns('',
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
